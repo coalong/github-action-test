d25451635b7a82a7e05020a7351c5f05d8825a9d
coalong <minay0904@gmail.com>
2022-07-05 18:04:44 +0900

    test: sonarQube gitHub action test

--- src/main/java/com/example/githubactiontest/test/Case.java
+++ src/main/java/com/example/githubactiontest/test/Case.java
@@ -1,4 +1,11 @@
 package com.example.githubactiontest.test;
 
 public class Case {
+    public static void main(String[] args) {
+        exampleMethod();
+    }
+
+    private static void exampleMethod() {
+        System.out.println("테스트 코드입니다.");
+    }
 }
---
+++ src/test/java/com/example/githubactiontest/test/CaseTest.java
@@ -0,0 +1,12 @@
+package com.example.githubactiontest.test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CaseTest {
+
+    @Test
+    void main() {
+    }
+}
